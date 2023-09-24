ed2c907db1cc6c01dba94a3429d9a1d3bc28cf70
bokehaachi <jagatharikanat55@gmail.com>
2023-09-24 20:10:15 +0530

    commit

--- java.js
+++ java.js
@@ -1,6 +1,6 @@
 ScrollReveal().reveal('section', {duration: 2000, reset: true});
 ScrollReveal().reveal('h2', {duration: 500, reset: true});
-ScrollReveal().reveal('p', {duration: 500, reset: true});
 ScrollReveal().reveal('header', {duration: 2000, reset: true});
 ScrollReveal().reveal('.gallery', {duration: 1000, reset: true});
+ScrollReveal().reveal('.about', {duration: 500, reset: true});
 
